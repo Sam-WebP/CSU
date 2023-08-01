1770ae2d28361de9136730fa0a6886d359e15fa3
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2023-08-01 22:55:26 +1000

    Final code with all the comments

--- ITC206 Programming In Java/Assessment1/Task1/Population.java
+++ ITC206 Programming In Java/Assessment1/Task1/Population.java
@@ -1,40 +1,48 @@
-package Assessment1.Task1;
+package Task1;
 import java.util.Scanner;
 
-// Calculates the projected population based on birth, death, and immigration rates.
 public class Population {
-    // Number of seconds in a year
+    
+    // Constant that represents the number of seconds in a year
     final static int YEARLY_SECONDS = 31536000;
     public static void main(String[] args) {
+        
+        // Scanner to read user input
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the seconds for every birth: ");
+        // Prompt user for the number of seconds between each birth
+        System.out.print("\nEnter the seconds for every birth: ");
         int birthInterval = input.nextInt();
 
-        System.out.print("Enter the seconds for every death: ");
+        // Prompt user for the number of seconds between each death
+        System.out.print("\nEnter the seconds for every death: ");
         int deathInterval = input.nextInt();
 
-        System.out.print("Enter the seconds for every new immigrant: ");
+        // Prompt user for the number of seconds between each immigrant
+        System.out.print("\nEnter the seconds for every new immigrant: ");
         int immigrantInterval = input.nextInt();
 
-        System.out.print("Enter the number of years: ");
+        // Prompt user for the number years to do the calculation across
+        System.out.print("\nEnter the number of years: ");
         int years = input.nextInt();
 
-        System.out.print("Enter the current population: ");
+        // Prompt user for the current population
+        System.out.print("\nEnter the current population: ");
         int population = input.nextInt();
 
-        // Calculate total births, deaths, and immigrants over the given years
+        // Calculate the number of births, deaths, and immigrants from the given number of years
         int totalBirths = totalOccurrences(birthInterval, years);
         int totalDeaths = totalOccurrences(deathInterval, years);
         int totalImmigrants = totalOccurrences(immigrantInterval, years);
         
-        // Predict the population after the given years
+        // Calculate the predicted population by adding the births and immigrants and then subtract the deaths
         int predictedPopulation = totalBirths - totalDeaths + totalImmigrants + population;
 
-        System.out.println("The population in " + years + " years is " + predictedPopulation);
+        // Show the results of the predicted population
+        System.out.print("\nThe population in " + years + " years is " + predictedPopulation + "\n");
     };
 
-    /*Calculate the total number of occurrences for an event in a given number of years*/
+    // Function that calculates the occurrences of an event given its interval in seconds and the amount of years
     public static int totalOccurrences(int secondInterval, int years) {
             int occurrences = (YEARLY_SECONDS * years) / secondInterval;
             return occurrences;
--- ITC206 Programming In Java/Assessment1/Task2/BookCalculator.java
+++ ITC206 Programming In Java/Assessment1/Task2/BookCalculator.java
@@ -4,50 +4,66 @@ import java.util.Scanner;
 public class BookCalculator {
 
     public static void main(String[] args) {
-
+        // Scanner to read user input
         Scanner input = new Scanner(System.in);
 
+        // totalBill variable to keep track of total bill amount.
         int totalBill = 0;
+        // response character is used to control the do-while loop
         char response = 'y';
 
+        // Welcome message
         System.out.println("\nWelcome to the Annual Australian Book Festival!");
 
+        // do-while loop runs until the user enters 'n' to stop calculating new bills.
         do {
+            // Number of large print hardback books purchased prompt.
             System.out.print("\nEnter the number of large print hardback books purchased: ");
             int largeQty = input.nextInt();
 
+            // Number of small print hardback books purchased prompt.
             System.out.print("Enter the number of small print hardback books purchased: ");
             int smallQty = input.nextInt();
 
+            // Number of softcover books purchased prompt.
             System.out.print("Enter the number of softcover books purchased: ");
             int softQty = input.nextInt();
             input.nextLine();
 
+            // Calculate number of discounted sets of books purchased
             int largeDealQty = largeQty / 2;
             int smallDealQty = smallQty / 3;
             int softDealQty = softQty / 4;
 
+            // Calculate number of books purchased at the standard price
             int largeStandardQty = largeQty % 2;
             int smallStandardQty = smallQty % 3;
             int softStandardQty = softQty % 4;
             
+            // Calculate total cost for each type of book
             int largeTotal = (largeDealQty * 30) + (largeStandardQty * 20);
             int smallTotal = (smallDealQty * 20) + (smallStandardQty * 10);
             int softTotal = (softDealQty * 15) + (softStandardQty * 5);
 
+            // Calculate total bill for this purchase
             int bill = largeTotal + smallTotal + softTotal;
             
+            // Add current bill to the total bill
             totalBill += bill;
 
+            // Display total bill
             System.out.println("Your total bill is: $" + bill + "\n");
 
+            // Ask user to choose if they want to calculate another bill
             System.out.print("Would you like to calculate another bill (y/n)? ");
             response = input.nextLine().charAt(0);
 
+            // Continue the loop when response does not equal 'n'
         } while (response != 'n');
 
-        System.out.print("\nTotal sales in this session: $" + totalBill);
-        System.out.print("\nGoodbye!\n");
+        // Display the total sales for the session
+        System.out.println("\nTotal sales in this session: $" + totalBill);
+        System.out.println("\nGoodbye!");
 
     }
 
