fe085de6fde04ba0ab70d93bc324d44a7c406a12
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2023-08-13 12:36:49 +1000

    Lessons on constructors, overloading and arrays of objects.

---
+++ ITC206 Programming In Java/BroCode/javaArrayOfObjects/Food.java
@@ -0,0 +1,11 @@
+package BroCode.javaArrayOfObjects;
+
+public class Food {
+
+    String name;
+
+    Food(String name) {
+        this.name = name;
+    }
+
+}
---
+++ ITC206 Programming In Java/BroCode/javaVariableScope/Main.java
@@ -0,0 +1,11 @@
+package BroCode.javaVariableScope;
+
+public class Main {
+    
+public static void main(String[] args) {
+
+    DiceRoller diceRoller = new DiceRoller();
+
+}
+
+}
