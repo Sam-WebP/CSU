1e7a374218042da818d4c1943c076cb25dbfcc53
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2023-08-19 16:36:50 +1000

    Lesson about string methods

--- ITC206 Programming In Java/BroCode/javaCopyObjects/Main.java
+++ ITC206 Programming In Java/BroCode/javaCopyObjects/Main.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) {
         
         Car car1 = new Car("Chevrolet", "Camaro", 2021);
-        Car car2 = new Car("Chevrolet", "Camaro", 2021);
+        // Car car2 = new Car("Chevrolet", "Camaro", 2021);
 
-        car2.copy(car1);
+        // car2.copy(car1);
+
+        Car car2 = new Car(car1);
 
         System.out.println(car1);
         System.out.println(car2);
---
+++ ITC206 Programming In Java/BroCode/javaStringMethods/Main.java
@@ -0,0 +1,32 @@
+package javaStringMethods;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+        String name = "Sam";
+
+        //boolean result = name.equalsIgnoreCase("sam");
+
+        // int result = name.length();
+
+        // char result = name.charAt(2);
+
+        // int result = name.indexOf("S");
+
+        // boolean result = name.isEmpty();
+
+        // String result = name.toUpperCase();
+
+        // String result = name.toLowerCase();
+
+        // String result = name.trim();
+
+        // String result = name.replace("S", "D");
+
+        System.out.println(result);
+    }
+    
+
+}
