3327cf9fa758a803c0027405a03371b2eb996225
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2023-08-21 19:13:32 +1000

    Lesson on the java wrapper classes

---
+++ ITC206 Programming In Java/BroCode/javaWrapperClasses/Main.java
@@ -0,0 +1,22 @@
+package javaWrapperClasses;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Sam";
+
+        if( c == '@') {
+            System.out.println("This is true");
+        } else {
+            System.out.println("This is false");
+        }
+
+    }
+
+
+}
