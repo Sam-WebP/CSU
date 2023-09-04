85b20951a3c2ccd806b1ba6b41fab66e6bf58517
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2023-09-05 09:07:40 +1000

    Created a directory to save my draw.io designs

--- ITC106 Programming Principles/Interact Exercises/Interact Exercises.py
+++ ITC106 Programming Principles/Interact Exercises/Interact Exercises.py
@@ -517,44 +517,362 @@ def main():
 if __name__ == "__main__":
     main() """
 
-###This is the second exercise from Topic 7###  
+# ###This is the second exercise from Topic 7###  
 
-def read_marks(file, line_num):
-    marks = file.readline(line_num + 3).strip()
-    if marks == "":
-        return None
-    else:
-        return int(marks)
+# def read_marks(file, line_num):
+#     marks = file.readline(line_num + 3).strip()
+#     if marks == "":
+#         return None
+#     else:
+#         return int(marks)
 
-def next_student(file, line_num):
-    line = file.readline(line_num)
-    if not line:
-        return False
-    else:
-        return True
+# def next_student(file, line_num):
+#     line = file.readline(line_num)
+#     if not line:
+#         return False
+#     else:
+#         return True
 
-def check_marks(mark):
-    if mark > 80:
-        return True
-    else:
-        return False 
+# def check_marks(mark):
+#     if mark > 80:
+#         return True
+#     else:
+#         return False 
     
-def display_students(file, line_num):
-    print(file.readline(line_num).strip())
-    print(file.readline(line_num + 1).strip())
-    print(file.readline(line_num + 2).strip())
-    print(file.readline(line_num + 3).strip())
+# def display_students(file, line_num):
+#     print(file.readline(line_num).strip())
+#     print(file.readline(line_num + 1).strip())
+#     print(file.readline(line_num + 2).strip())
+#     print(file.readline(line_num + 3).strip())
+
+# def main():
+#     file = open('student.txt', 'r')
+#     line_num = 1
+#     while (next_student(file, line_num)):
+#         mark = int(read_marks(file, line_num))
+#         if check_marks(mark):
+#             display_students(file, line_num)
+#         line_num += 4
+
+# main()
+    
+# ###This is a random exercise from Topic 8###  
+
+# """
+# The program calculates average of rainfall data provided in a text file.
+# """
+
+# # Extend the program with the following features
+# # 1. Find out on what date the maximum rainfall occurred.
+# # 2. Display all dates on which non-zero rainfall was recorded.
+
+# def main():
+#     rain_data = file_to_list('rain.txt')
+#     if rain_data == -1:  # was there an error?
+#         print('Invalid data found in file')
+#         return
+
+#     print(rain_data)  # for testing
+
+#     avg_rain = list_average(rain_data)
+
+#     print('Average rainfall in given period:', avg_rain)
+#     max_rain_date(rain_data)
+#     rain_days(rain_data)
+    
+
+
+# def file_to_list(file_name):
+#     """
+#     A generic function that opens file_name, reads each line,
+#     converts to float and stores in the list. The list is returned
+#     """
+#     file_data = []  # start with empty list
+
+#     try:
+#         file = open(file_name, 'r')
+
+#         for line in file:
+#             file_data.append(float(line))
+
+#     except ValueError:
+#         return -1  # instead of a list
+#     finally:
+#         file.close()
+
+#     return file_data  # the populated list
+
+
+# def list_average(list_param):
+#     """
+#     A generic function that returns the average of values found
+#     in list_param. The list is expected to have numeric values.
+#     """
+#     sum = 0
+#     for value in list_param:
+#         sum += value
+#     # Alternate: could have used sum(list_param) here
+
+#     return sum / len(list_param)
+
+# def max_rain_date(rain_data):
+#     print(f"The {rain_data.index(max(rain_data)) + 1}th day of dec had the highest rainfall")
+
+# def rain_days(rain_data):
+#     rained = []
+#     for i, rain in enumerate(rain_data):
+#         if rain > 0:
+#             day = i + 1
+#             suffix = "th" if 11 <= day <= 13 else {1:"st", 2:"nd", 3:"rd"}.get(day % 10, "th")
+#             rained.append(f"{day}{suffix}")
+#     print(f"It rained on the following days: {', '.join(rained)}")
+
+# main()
+
+# ###This is the first exercise from Topic 8### 
+
+# import random
+
+# def get_number_of_rolls():
+#     return int(input("How many times do you want to roll the dice? "))
+
+# def roll_dice():
+#     return random.randint(1, 6)
+
+# def roll_dice_multiple_times(times):
+#     return [roll_dice() for _ in range(times)]
+
+# def main():
+#     number_of_rolls = get_number_of_rolls()
+#     rolls = roll_dice_multiple_times(number_of_rolls)
+#     print(rolls)
+
+# main()
+
+# ###This is the second exercise from Topic 8### 
+
+# students = []
+
+# def how_many_students():
+#     return int(input("How many students are you going to enter? "))
+
+# def store_new_info(array, message):
+#     return array.append(input(message))
+
+# def validate_marks(mark):
+#     print(f"{mark} is what the mark is")
+#     try:
+#         0 > mark > 100
+#     except:
+#         print("That is not a valid mark, try again.")
+#         students.remove(students[-1])
+#         store_new_info(students, "Marks: ")
+    
+# def get_student_data():
+#     print("Please enter the following student information: ")
+#     store_new_info(students, "ID: ")
+#     store_new_info(students, "Last Name: ")
+#     store_new_info(students, "First Name: ")
+#     store_new_info(students, "Marks: ")
+#     validate_marks(students[-1])
+#     return students
+
+# def main():
+#     size = how_many_students()
+
+#     for stu in range(size):
+#         get_student_data()
+#     print(students)
+
+# main()
+    
+# ###This is the first exercise from Topic 9### 
+
+# Date formatting
+# Get a date as input from the user in the format dd/mm/yyyy. Convert and print the date in this format: Jan 5, 2019.
+
+# date = str(input("Insert a date in the format dd/mm/yyyy: "))
+# date_split = date.split('/')
+# month = date_split[1]
+# months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
+# def sort_num(mm):
+#     if int(mm) < 10:
+#         str(mm).strip("0")
+#     return int(mm)
+# idx = sort_num(month) - 1
+# print(F"The final answer should be: {months[idx]} {date_split[0]}, {date_split[2]}.")
+
+# ###This is the second exercise from Topic 9###
+
+# Write a program that processes a text file and creates a new file in which all three-letter secret agency names like CIA, FBI, NSA have been censored to ***.
+
+# secret_agencies = ["CIA", "FBI", "NSA"]
+# secrets_present = []
+# first_file = open("agencies.txt" , 'r')
+
+# #function to go through file and search through an array to see if values are there
+# def check_file(f_name, arr_match, arr_append):
+#     for line in f_name:
+#         if line.strip() in arr_match:
+#             arr_append.append(line)
+
+# #Count items in list then print "***" for those instances on a new line in a new file
+# def new_file(n_file, arr_append):
+#     second_file = open(n_file, "w")
+#     for _ in range(len(arr_append)):
+#         second_file.write(f"***\n")
+    
+# def main():
+#     check_file(first_file, secret_agencies, secrets_present)
+#     new_file("secret_agencies.txt", secrets_present)
+
+# main()
+
+# ###This is the first exercise from Topic 10### 
+
+# 1) Prompt the user to select from four options: 
+#   - Enter an already existing Vegetable name
+#   - Add a new vegetable and price
+#   - Update an existing vegetable price
+#   - Delete an existing vegetable and its price
+
+# vegetables = {
+#     "Carrot": 5,
+#     "Broccoli": 6,
+#     "Garlic": 2,
+#     "Kale": 4
+# }
+
+# inputs = []
+
+# def main():
+#     show_menu()
+#     get_input()
+#     option_selector(inputs[0], inputs[1])
+
+# def get_input():
+#     option = input("Select from the above options (Type A, B, C or D): ")
+#     vegetable = input("What vegetable are you enquiring about? ")
+#     inputs.clear()
+#     inputs.append(option)
+#     inputs.append(vegetable)
+
+# def show_menu():
+#     print("\nA: Find vegetable price")
+#     print("B: Add a new vegetable")
+#     print("C: Update an existing vegetable price")
+#     print("D: Delete an existing vegetable and its price \n")
+
+# def option_selector(option, vegetable):
+#     if option == "A":
+#         option_a(vegetable)
+#     elif option == "B":
+#         option_b(vegetable)
+#     elif option == "C":
+#         option_c(vegetable)
+#     elif option == "D":
+#         option_d(vegetable)
+
+# def option_a(vegetable):
+#     price = vegetables.get(vegetable, -1)
+#     if price == -1:
+#         print(f"{vegetable} does not already exist in the system!")
+#         main()
+#     else:
+#         print(f"{vegetable} has a price of ${price}")
+#         main()
+    
+# def option_b(vegetable):
+#     price = int(input(f"What should the price of {vegetable} be? "))
+#     print(f"This is what the price is that you just entered: {price}")
+#     vegetables[vegetable] = price
+#     print(f"Thank you for adding {vegetable} into the system")
+#     main()
+
+# def option_c(vegetable):
+#     current_price = vegetables.get(vegetable, -1)
+#     if current_price == -1:
+#         print(f"{vegetable} does not already exist in the system!")
+#         main()
+#     else:
+#         print(f"The current price of {vegetable} is ${current_price}")
+#         new_price = input("What do you want to change the price to? ")
+#         vegetables[vegetable] = new_price
+#         print(f"You have updated the price of {vegetable} to ${new_price}")
+#         main()
+    
+# def option_d(vegetable):
+#     if vegetables.get(vegetable, -1) == -1:
+#         print(f"{vegetable} does not already exist in the system!")
+#         main()
+#     else:
+#         vegetables.pop(vegetable)
+#         print(f"You have successfully removed the item {vegetable}")
+#         main()
+
+# main()
+
+# ###This is the second exercise from Topic 10### 
 
 def main():
-    file = open('student.txt', 'r')
-    line_num = 1
-    while (next_student(file, line_num)):
-        mark = int(read_marks(file, line_num))
-        if check_marks(mark):
-            display_students(file, line_num)
-        line_num += 4
+    word = input("Search for a words definition: ")
+    verify_existence(word)
+    list_definitions(word)
 
+words = {
+    "Abandon": ("To give up or relinquish control of, to surrender or to give oneself over, or to yield to one's emotions.",
+              "To desist in doing, practising, following, holding, or adhering to; to turn away from; to permit to lapse; to renounce; to discontinue.",
+              "To surrender to the insurer (an insured item), so as to claim a total loss."),
+    "Engage": ("To engross or hold the attention of; to keep busy or occupied.",
+               "To arrange to employ or use (a worker, a space, etc.)"),
+    "Liable": ("bound or obliged in law or equity; responsible; answerable."),
+    "Shorthand": ("A rough and rapid method of writing by substituting symbols for letters, words, etc.",
+                  "Any brief or shortened way of saying or doing something."),
+    "Symbol": ("A character or glyph representing an idea, concept or object.",
+               "Any object, typically material, which is meant to represent another (usually abstract) even if there is no meaningful relationship.",
+               "Visible traces or impressions, made using a writing device or tool, that are connected together and/or are slightly separated.")
+}
+
+def verify_existence(w):
+    if words.get(w, -1) == -1:
+        print(f"Sorry '{w}' does not exist in the dictionary")
+
+def list_definitions(w):
+    i = 1
+    for i in range(len(words[w])):
+        print(f"\n{i + 1}. {words[w][i]}\n")
+        i += 1
+        
 main()
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
