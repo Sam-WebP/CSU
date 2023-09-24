021340f6003ac72170d47155b95df9694dfd2a2b
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2023-09-24 11:12:30 +1000

    Created the Octagon class for task 2

---
+++ ITC206 Programming In Java/Assessment3/Task2/testOctagon.java
@@ -0,0 +1,16 @@
+package Assessment3.Task2;
+
+public class testOctagon {
+    
+    public static void main(String[] args) {
+
+        Octagon octagonTest = new Octagon(5);
+
+        System.out.println("Area = " + octagonTest.getArea());
+        System.out.println("Perimeter = " + octagonTest.getPerimeter());
+
+        
+
+    }
+
+}
