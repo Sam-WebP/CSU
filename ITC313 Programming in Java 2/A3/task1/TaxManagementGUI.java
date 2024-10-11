8ce7d50ac60bf91efe85edf91b4e25d425fbf24e
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2024-10-11 22:03:53 +1100

    feat: create main JavaFX application class for Tax Management GUI

---
+++ ITC313 Programming in Java 2/A3/task1/TaxManagementGUI.java
@@ -0,0 +1,26 @@
+package task1;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TaxManagementGUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("tax_management.fxml"));
+        Parent root = loader.load();
+        TaxManagementController controller = loader.getController();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Tax Management Application");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
